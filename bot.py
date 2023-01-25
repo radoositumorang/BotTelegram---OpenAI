92402585d0a50be5c4e5351ce1e501ed07693b52
Rado Nelson Dela Sadrak Situmorang <89694183+radoositumorang@users.noreply.github.com>
2023-01-26 00:19:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import asyncio
 from telebot.async_telebot import AsyncTeleBot
 
 # masukan api key dari open AI dan api token dari Botfather yang di butuhkan
-openai.api_key = "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"
-bot = AsyncTeleBot('5535583805:AAH626N8IZwjN2pPnLDfjEVhPZZ8ehDb7wk')
+openai.api_key = "Openai"
+bot = AsyncTeleBot('botfathertoken')
 
 
 @bot.message_handler(commands=['help', 'start'])
